a654310132e9d85e962ef34f3f43749dd605c7cb
AntorAcs2239 <nothingtodoacs@gmail.com>
2023-01-14 11:03:09 +0600

    Version 2

--- Bionomial Cofficient.cpp
+++ Bionomial Cofficient.cpp
@@ -1,43 +1,63 @@
+// Program about Passcal Triangle using Binomial Cofficient
 #include<bits/stdc++.h>
 using namespace std;
-int k=11;
-int max=400;
 int main()
 {
-    int n=11,k=11;
-    int pas[n+1][k+1];
-    for(int i=0;i<=n;i++)
+    int row,column;
+    cout<<"Enter the Value of row and column"<<endl;
+    cin>>row>>column;
+    row--;
+    column--;
+    int pascal[row+1][column+1];
+    //Base Cases 
+    for(int i=0;i<=row;i++)
     {
-        for(int j=1;j<=k;j++)pas[i][j]=0;
+        for(int j=1;j<=column;j++)pascal[i][j]=0;
     }
-    for(int i=0;i<=n;i++)
+    for(int i=0;i<=row;i++)
     {
-        pas[i][0]=1;
+        pascal[i][0]=1;
     }
-    for(int i=0;i<=n;i++)
+    for(int i=0;i<=row;i++)
     {
-        for(int j=0;j<=k;j++)
+        for(int j=0;j<=column;j++)
         {
             if(i==j)
             {
-                pas[i][j]=1;
+                pascal[i][j]=1;
             }
         }
     }
-    for(int i=1;i<=n;i++)
+    //Base Cases 
+
+
+    // calculate All other values of matrix
+    for(int i=1;i<=row;i++)
     {
-        for(int j=1;j<=k;j++)
+        for(int j=1;j<=column;j++)
         {
-            pas[i][j]=pas[i-1][j]+pas[i-1][j-1];
+            pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
         }
     }
-    for(int i=0;i<=n;i++)
+    for(int i=0;i<=row;i++)
     {
         for(int j=0;j<=i;j++)
         {
-            cout<<pas[i][j]<<" ";
+            cout<<pascal[i][j]<<" ";
         }
         cout<<endl;
     }
+    int q;
+    cout<<"Enter the number of query"<<endl;
+    cin>>q;
+    while(q--)
+    {
+        int n,r;
+        cout<<"Enter the value of n and r to find n_c_r"<<endl;
+        cin>>n>>r;
+        cout<<"Value of n_c_r is: "<<pascal[n][r]<<endl;
+    }
+    cin>>q;
+
 
 }
